8b49bb00c80c2dbf810883d90645c21286409344
vVviewVv <viewissara1246@gmail.com>
2021-03-09 16:26:38 +0700

    fix heroku

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
 import express from 'express'
+import fs from 'fs'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { body, query, validationResult } from 'express-validator'
 
+interface DbSchema {
+  users: User[]
+}
+interface User {
+  username: string
+  password: string
+  firstname: string
+  lastname: string
+  balance: number
+}
 interface JWTPayload {
   username: string;
-  password: string;
+}
+const readDbFile = (): DbSchema => {
+  const raw = fs.readFileSync('db.json', 'utf8')
+  const db: DbSchema = JSON.parse(raw)
+  return db
 }
 
 const app = express()
@@ -16,6 +31,7 @@ app.use(cors())
 const PORT = process.env.PORT || 3000
 const SECRET = "SIMPLE_SECRET"
 const info = require("./user_info");
+
 
 
 app.post("/register", (req, res) => {
@@ -43,58 +59,65 @@ app.post("/register", (req, res) => {
 
 app.post('/login',
   (req, res) => {
+
     const { username, password } = req.body
     // Use username and password to create token.
-    const user = info.find(user => user.username === username)
-  if (!user) {
-    res.status(400)
-    res.json({ message: 'Invalid username or password' })
-    return
-  }
-  if (!bcrypt.compareSync(password, user.password)) {
-    res.status(400)
-    res.json({ message: 'Invalid username or password' })
-    return
-  }
-  const token = jwt.sign({username}) as JWTPayload
-    return res.status(200).json({
-      message: 'Login succesfully',
-      token
-    })
+    const user = info.users.find((data : any) => data.username === username)
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid username or password' })
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(400).json({ message: 'Invalid username or password' })
+     
+    }
+    const token = jwt.sign({username : user.username,password : user.password } as JWTPayload , SECRET)
+    return res.status(200).json({ message:"Login successfully", token})
   })
 
+  app.get('/balance',
+  (req, res) => {
+    const token = req.query.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      const db = readDbFile()
+      const reqUser = db.users.find(user => user.username === username)
+      res.status(200).json({name: reqUser?.firstname + " " + reqUser?.lastname,balance: reqUser?.balance})
+    }
+    catch (e) {
+      //response in case of invalid token
+      res.status(401).json({
+        message : "Invalid token"
+      })
+    }
+  })
 
-app.get("/balance", (req, res) => {
-  const token = req.query.token as string
-  try {
-    const { username } = jwt.verify(token, SECRET) as JWTPayload
-    const user = users.find(user => user.username === username)
-    res.status(200);
-    res.json({"name": username,"balance": user?.balance});
-  }
-  catch (error) {
-    res.status(401);
-    res.json({ message: "Invalid token"});
-    return
-  }
-});
+app.post('/deposit',
+  (req, res) => {
+    try{
+      const {amount} = req.body
+      const db = readDbFile()
+      //Is amount <= 0 ?
+      if (amount <= 0)
+      return res.status(400).json({ message: "Invalid data" })
 
-app.post('/deposit',body('amount').isInt({ min: 1 }),(req, res) => {
-  const token = req.query.token as string
-  if (!validationResult(req).isEmpty())
-    return res.status(400).json({ message: "Invalid data" })
-  try {
-    const { username } = jwt.verify(token, SECRET) as JWTPayload
-    const user = users.find(user => user.username === username)
-    res.status(200);
-    res.json({message: "Deposit successfully","balance": 200});
-  }
-  catch (error) {
-    res.status(401);
-    res.json({ message: "Invalid token"});
-    return
-  }
-});
+      const token: string = req.query.token as string
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      const reqUser = db.users.find(user => user.username === username)
+      if(reqUser){
+        reqUser.balance = reqUser.balance + amount
+        fs.writeFileSync("db.json", JSON.stringify(db,null,2))
+        return res.status(200).json({
+            message: "Deposit successfully",
+            balance: reqUser?.balance 
+        })
+      }
+      res.status(404).json({message:"USER NOT FOUND"})
+    }catch(e){
+      res.status(401).json({
+        message : "Invalid token"
+      })
+    }
+  })
 
 app.post('/withdraw',(req, res) => {
   const token = req.headers.authorization
@@ -110,7 +133,7 @@ app.post('/withdraw',(req, res) => {
       return
     }
     const { username } = jwt.verify(token, SECRET) as JWTPayload
-    const balance = users.find(user=>user.username===username)?.balance
+    const balance = info.find(info=>user.username===username)?.balance
     const newbalance = balance - amount
     if(newbalance<0){
       res.status(400)
